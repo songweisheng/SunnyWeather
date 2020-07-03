8d13f991bbd640e29ecbe80d07815c2bc8d4ac4d
songweisheng <tony@gmail.com>
2020-07-04 01:12:23 +0800

    First commit.

---
+++ .IntelliJIdea2018.3/system/tmp/intellij-ssh-askpass.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+"C:/Users/SWSH/Desktop/宋维晟/IntelliJ IDEA 2018.3.4/jre64/bin/java" -cp "C:/Users/SWSH/Desktop/宋维晟/IntelliJ IDEA 2018.3.4/plugins/git4idea/lib/git4idea-rt.jar;C:/Users/SWSH/Desktop/宋维晟/IntelliJ IDEA 2018.3.4/lib/xmlrpc-2.0.1.jar;C:/Users/SWSH/Desktop/宋维晟/IntelliJ IDEA 2018.3.4/lib/commons-codec-1.10.jar;C:/Users/SWSH/Desktop/宋维晟/IntelliJ IDEA 2018.3.4/lib/util.jar" org.jetbrains.git4idea.nativessh.GitNativeSshAskPassApp "$@"
